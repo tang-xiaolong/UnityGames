f279c63e2a1cde3db6cf4ba6800753bb0978cb21
Dragon <1097145586@qq.com>
2019-09-08 23:04:02 +0800

    上传拼图游戏管理脚本框架

---
+++ Assets/Scripts/Element.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Element : MonoBehaviour
+{
+    private Sprite sprite;
+    private int id;
+    private int pos;
+    void Start()
+    {
+        
+    }
+
+    public void SetSprite(Sprite sprite)
+    {
+        this.sprite = sprite;
+    }
+    public void SetId(int id)
+    {
+        this.id = id;
+    }
+    public void SetPos(int pos)
+    {
+        this.pos = pos;
+    }
+}
---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+
+    //1、读取图片
+    //2、输入切割大小
+    //3、切割图片
+    //4、打乱图片
+    private static Texture imageSource;
+    private List<Element> pictures;
+    private int size = 0;
+
+    //TODO:remove static attribute when game runing
+    [MenuItem("QuickTool/PSDImport ...", false, 1)]
+    public static void OpenFile()
+    {
+        string inputFile = EditorUtility.OpenFilePanel("Choose Image", Application.dataPath, "jpg");
+        if (!string.IsNullOrEmpty(inputFile))
+        {
+            Debug.Log(inputFile);
+            string fileName;
+            CopyToResources(inputFile,out fileName);
+            AssetDatabase.Refresh();
+            ReadImage(fileName);
+        }
+        GC.Collect();
+    }
+    public static void CopyToResources(string path,out string fileName)
+    {
+        string[] res = path.Split('/');
+        fileName = res[res.Length - 1];
+        string resPath = Application.dataPath + "/Resources/"+fileName;
+        File.Copy(path, resPath, true);
+    }
+    public static void ReadImage(string path)
+    {
+        Debug.Log(path);
+        string fileName = path.Split('.')[0];
+        imageSource = Resources.Load<Texture>(fileName);
+        if (imageSource == null)
+        {
+            Debug.Log("null");
+            return;
+        }
+        TextureImporter import = AssetImporter.GetAtPath("Resources/"+path) as TextureImporter;
+        import.textureType = TextureImporterType.Sprite;
+        import.spriteImportMode = SpriteImportMode.Multiple;
+        //TODO:
+    }
+    public void GetCount()
+    {
+
+    }
+    public void SliceImage(int count)
+    {
+
+    }
+    public void RandomImage()
+    {
+
+    }
+    public void SwapElement()
+    {
+
+    }
+}
