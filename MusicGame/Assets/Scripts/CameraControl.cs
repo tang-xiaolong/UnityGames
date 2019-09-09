7de940aaa3d04bcb18dedc0a42870cb75cd5a571
唐小龙 <tangxiaolong.gw>
2019-09-09 21:17:20 +0800

    音频可视化游戏

---
+++ MusicGame/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+	public enum MyCameraType
+	{
+		Circle,
+		Line
+	}
+
+	public Transform center;
+	private float switchTime = 3;
+	private float rotateSpeed = 40f;
+	private float high = 50f;
+	private Vector3 pos;
+	public MyCameraType type = MyCameraType.Circle;
+    private Control control;
+    private int count;
+
+    void Start()
+    {
+        control = FindObjectOfType<Control>();
+        count = control.cubeCount;
+        //transform.position = 
+        if (type == MyCameraType.Circle)
+		{
+			StartCoroutine(Rotate());
+		}
+		else
+		{
+			LineInit();
+		}
+		pos = center.position;
+		//pos.y = transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (type == MyCameraType.Circle)
+		{
+			transform.RotateAround(pos, Vector3.up, rotateSpeed * Time.deltaTime);
+            transform.LookAt(center);
+		}
+		//transform.Rotate(pos, rotateSpeed * Time.deltaTime,Space.Self);
+		//transform.Rotate((transform.position - center.position).normalized, rotateSpeed * Time.deltaTime);
+		//transform.Rotate(center.position,rotateSpeed*Time.deltaTime);
+    }
+	public void LineInit()
+	{
+		transform.position = new Vector3(0, 0, -count);
+		transform.LookAt(center);
+	}
+
+	IEnumerator Rotate()
+	{
+        //设置摄像机的位置
+        
+		//transform.position = new Vector3(62f, 42f, -5f)+new Vector3(0,0,0);
+		transform.position = new Vector3(count, count, -5f);
+		transform.rotation = new Quaternion(28, -81, 0, 0);
+		while(true)
+		{
+			yield return new WaitForSeconds(switchTime);
+			//改变摄像机的旋转方向
+			if (Random.Range(0, 2) == 1)
+			{
+				rotateSpeed *= -1;
+				switchTime = Random.Range(3, 7);
+			}
+		}
+	}
+}
---
+++ MusicGame/Assets/Scripts/Control.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Control : MonoBehaviour
+{
+	private AudioSource myAudio;
+	public int cubeCount = 64;
+	private float[] info;
+	public GameObject cube;
+	public CameraControl.MyCameraType type = CameraControl.MyCameraType.Circle;
+    private CameraControl cameraControl;
+	
+	private List<Transform> cubes = new List<Transform>();
+	private List<Material> materials = new List<Material>();
+	private float radius = 30;
+	private void Init()
+	{
+        cameraControl = FindObjectOfType<CameraControl>();
+        cameraControl.type = type;
+		myAudio = GetComponent<AudioSource>();
+		info = new float[cubeCount];
+	}
+	/// <summary>
+	/// 圆形
+	/// </summary>
+	private void Circle()
+	{
+		Vector3 pos = transform.position;
+		float angle = 0f;
+		float dat = 360 / cubeCount;
+		for (angle = 0; angle < 360f; angle += dat)
+		{
+			float x = pos.x + radius * cubeCount / 64 * Mathf.Cos(angle * Mathf.PI / 180f);
+			float z = pos.z + radius * cubeCount / 64 * Mathf.Sin(angle * Mathf.PI / 180f);
+			GameObject go = Instantiate(cube);
+			go.SetActive(true);
+			cubes.Add(go.transform);
+			materials.Add(go.GetComponent<Renderer>().material);
+			go.transform.position = new Vector3(x, 0, z);
+			go.transform.parent = transform;
+		}
+	}
+	/// <summary>
+	/// 直线型
+	/// </summary>
+	private void Line()
+	{
+		Vector3 pos = transform.position;
+		for (int i = 0; i < cubeCount; i++)
+		{
+			GameObject go = Instantiate(cube);
+			go.SetActive(true);
+			cubes.Add(go.transform);
+			materials.Add(go.GetComponent<Renderer>().material);
+			go.transform.position = new Vector3(i - cubeCount + pos.x + i, -40, 30);
+			go.transform.parent = transform;
+		}
+	}
+	private void Awake()
+	{
+		Init();
+		//Circle();
+		if (type == CameraControl.MyCameraType.Circle)
+		{
+            Circle();
+		}
+		else
+			Line();
+	}
+	private void Update()
+	{
+		Cube();
+	}
+	public void Cube()
+	{
+		myAudio.GetSpectrumData(info, 0, FFTWindow.Rectangular);
+		for (int i = 0; i < cubeCount; i++)
+		{
+			//设置立方体的规模，设置立方体的高度scale
+			//Debug.Log(info[i]);
+			float myY = info[i] * (i + 1) * (i + 1);
+			myY = Mathf.Clamp(myY,1,20);
+			Debug.Log(myY);
+			SetColor(i,myY);
+			cubes[i].localScale = new Vector3(1, myY + 1, 1);
+			cubes[i].position = new Vector3(cubes[i].position.x, myY / 2 + 0.5f, cubes[i].position.z);
+		}
+	}
+	public void SetColor(int index,float myY)
+	{
+		if (myY > 9)
+		{
+			materials[index].SetColor("_EmissionColor", new Color(0 / 255f, 11 / 255f, 233 / 255f));
+		}
+		else if (myY > 7)
+		{
+			materials[index].SetColor("_EmissionColor", new Color(0 / 255f, 233 / 255f, 4 / 255f));
+		}
+		else if (myY > 5)
+		{
+			materials[index].SetColor("_EmissionColor", new Color(233 / 255f, 93 / 255f, 0 / 255f));
+		}
+		else if (myY > 3)
+		{
+			materials[index].SetColor("_EmissionColor", new Color(231 / 255f, 0 / 255f, 5 / 255f));
+		}
+		else
+		{
+			materials[index].SetColor("_EmissionColor", new Color(233 / 255f, 233 / 255f, 233 / 255f));
+		}
+	}
+}
